d16b47fb0bbbde11a68a9f2858795817bde86c3d
Daisuke Maki <lestrrat+github@gmail.com>
2022-01-06 22:32:51 +0900

    Tweaks

--- README.md
+++ README.md
@@ -5,11 +5,43 @@ elements out of a container. It uses the new type parameters introduced in go 1.
 
 # Slices
 
-```go
-c := choose.Slice(src)
-// Choose one random element
-elem := c.One()
+Initialize a choose.SliceChooser:
 
-// Choose N random elements
+```go
+src := []int{....}
+c := choose.Slice[int](src)
+```
+
+## Choose a random element from a slice
+
+```go
+elem := c.One()
+```
+
+## Choose N random elements from a slice
+
+```go
 elems := c.N(3)
+```
+
+# Map
+
+Initialize a choose.MapChooser:
+
+```go
+src := map[string]int{...}
+c := choose.Map[string,int](src)
+```
+
+## Choose a random element (as choose.MapElement) from a map
+
+```go
+elem := c.One()
+fmt.Printf("%s -> %v", elem.Key, elem.Value)
+```
+
+## Choose N random elements (as []choose.MapElement) from a map
+
+```go
+elem := c.N(3)
 ```
--- choose.go
+++ choose.go
@@ -27,15 +27,20 @@ func Slice[T any](src []T) *SliceChooser[T] {
 	}
 }
 
-// One chooses one element from the source slice `src`
+// One chooses one element from the source slice
 func (c *SliceChooser[T]) One() T {
 	return c.src[c.rng.Intn(c.n)]
 }
 
 // N chooses as many elements specified by `howmany` parameter
-// from the source slice `src`
+// from the source slice.
+// If the `howmany` exceeds the number of elements in the 
+// slice, the entire slice is returned
 func (c *SliceChooser[T]) N(howmany int) []T {
 	indices := c.rng.Perm(c.n)
+	if howmany > c.n {
+		howmany = c.n
+	}
 	dst := make([]T, howmany)
 	for i, idx := range indices {
 		if i >= howmany {
@@ -46,30 +51,41 @@ func (c *SliceChooser[T]) N(howmany int) []T {
 	return dst
 }
 
+// MapChooser is used to choose random elements from a map
 type MapChooser[K comparable, V any] struct {
 	rng *rand.Rand
 	keys []K
 	src map[K]V
 	n int
 }
+
 type MapElement[K comparable, V any] struct {
 	Key K
 	Value V
 }
 
+// Map creates a MapChooser specialized for a map[K]V.
+// Upon calling this function, the elements from `src` are
+// copied into a local storage: thus modifying `src` does
+// not affect the subsequent operations.
 func Map[K comparable, V any](src map[K]V) *MapChooser[K,V] {
 	l := len(src)
 	keys := make([]K, 0, l)
+	elements := make(map[K]V)
 	for k := range src {
 		keys = append(keys, k)
+		elements[k] = src[k]
 	}
+
 	return &MapChooser[K,V]{
 		rng: rand.New(rand.NewSource(time.Now().UnixNano())),
 		keys: keys,
+		src: src,
 		n: l,
 	}
 }
 
+// One chooses one element from the source map
 func (c *MapChooser[K,V]) One(src map[K]V) MapElement[K,V] {
 	key := c.keys[c.rng.Intn(c.n)]
 	return MapElement[K,V]{
@@ -78,8 +94,17 @@ func (c *MapChooser[K,V]) One(src map[K]V) MapElement[K,V] {
 	}
 }
 
+// N returns a slice of choose.MapElement with `howmany`
+// elements, randomly chosen from the original map.
+//
+// If `howmany` exceeds the number of entries in the
+// original map, the entire map is returned.
 func (c *MapChooser[K,V]) N(howmany int) []MapElement[K,V] {
 	indices := c.rng.Perm(c.n)
+	if howmany > c.n {
+		howmany = c.n
+	}
+
 	dst := make([]MapElement[K,V], howmany)
 	for i, idx := range indices {
 		if i >= howmany {
